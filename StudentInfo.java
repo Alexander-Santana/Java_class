ff74524a163f84555229921fb2fd7a4534a29416
Alexander Santana <108630753+Alexander-Santana@users.noreply.github.com>
2024-10-25 16:45:20 -0400

    Add files via upload
    a cool app I made that acts like its getting student info/grades

---
+++ StudentInfo.java
@@ -0,0 +1,31 @@
+//Chapter 2, Pearson, Lab Assignment 1
+//Filename: StudentIntro
+//Programmer: Alexander Santana
+//Date: 9/9/2024
+
+public class StudentInfo
+{
+   public static void main(String[] arg)
+   {
+      char firstInitial = 'A';
+      char lastInitial = 'S';
+      double gradeIWant = 3.5; 
+      
+      //This is the top border
+      System.out.println("-----------------------------------------------------------------");
+      //This is my first and last name
+      System.out.println("Alexander Santana");
+      //This is my major
+      System.out.println("Information Technology Computer Prog. and Development AAS");
+      //This is my first initial stored in a variable
+      System.out.println(firstInitial);
+      //This is my last initial stored in a variable
+      System.out.println(lastInitial);
+      //This is the GPA I would like
+      System.out.println(gradeIWant);
+      //This why im taking the class
+      System.out.println("It's a requirement, also learning new coding languages is fun.");
+      //This is the bottom border
+      System.out.println("-----------------------------------------------------------------");
+   }
+}
